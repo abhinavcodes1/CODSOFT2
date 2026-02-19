9f673decc714097100460651afefaed071076721
Abhinav Singh <100156216+abhinavcodes1@users.noreply.github.com>
2026-02-19 23:20:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+let display = document.getElementById("display");
+
+function append(value){
+    display.value += value;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function deleteLast(){
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }catch{
+        display.value = "Error";
+    }
+}
+
+/* Keyboard Support */
+document.addEventListener("keydown", function(e){
+    if("0123456789+-*/.%".includes(e.key)){
+        append(e.key);
+    }
+
+    if(e.key === "Enter"){
+        calculate();
+    }
+
+    if(e.key === "Backspace"){
+        deleteLast();
+    }
+    
+    if(e.key === "Escape"){
+        clearDisplay();
+    }
+});
